b75c5d9cd463e69163b261a47b605e313d48850a
Mauricio Carvalho Pinheiro <mauricio.pinheiro@pontaltech.com.br>
2024-01-13 13:49:57 -0300

    chore: ensure YAGNI

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,11 +1,9 @@
 import './config/module-alias'
 import { env } from '@/main/config/env'
-import { MySQLConnection } from '@/infra/repos/mysql/helpers'
 import { app } from '@/main/config/app'
 
 import { logger } from '@/infra/helpers'
 
 import 'reflect-metadata';
 
-MySQLConnection.getInstance()
 app.listen(env.port, () => logger.success(`Server running at http://localhost:${env.port}`))

7d46d197439d87f4e2ac4cb86e92fa05de75dd69
Mauricio Carvalho Pinheiro <mauricio.pinheiro@pontaltech.com.br>
2024-05-06 11:00:59 -0300

    fix: change docs paths

--- src/application/main/docs/routes.ts
+++ src/application/main/docs/routes.ts
@@ -3,8 +3,7 @@
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 import { Controller, ValidationService, FieldErrors, ValidateError, TsoaRoute, HttpStatusCodeLiteral, TsoaResponse, fetchMiddlewares } from '@tsoa/runtime';
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-import { HealthCheckDoc } from './../../../../docs/healthCheckDoc';
-import { expressAuthentication } from './../middlewares/authentication';
+import { HealthCheckDoc } from '@/infra/docs/healthCheckDoc';
 // @ts-ignore - no great way to install types from subpackage
 const promiseAny = require('promise.any');
 import type { RequestHandler } from 'express';
